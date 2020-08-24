9001f9618442e3879128fa71fe505e70dcd57990
Ramunas <rambek@gmail.com>
2020-08-24 21:32:19 +0300

    Final version

--- src/Components/Currency.js
+++ src/Components/Currency.js
@@ -5,69 +5,79 @@ import Axios from 'axios';
 
 const Currency = () => {
 
+    //set local states
     const [currencies, setCurrencies] = useState();
+    const [firstValue, setFirstValue] = useState(1);
+    const [secondValue, setSecondValue] = useState(1);
+    const [firstCurrency, setFirstCurrency] = useState("EUR");
+    const [secondCurrency, setSecondCurrency] = useState("EUR");
 
+    //get api with currencies and rates
     useEffect(() => {
         Axios.get('https://api.exchangeratesapi.io/latest?base=EUR').then(({ data }) => {
             setCurrencies(data);
         })
     }, []);
 
-    const [firstValue, setFirstValue] = useState(1);
-    const [secondValue, setSecondValue] = useState(1);
-
-    const [fromCurrency, setFirstCurrency] = useState("EUR");
-    const [secondCurrency, setSecondCurrency] = useState("EUR");
-
-    const FirstCurrencyChange = event => {
+    //takes currencies and values 
+    const FirstCurrencyChange = (event) => {
         setFirstCurrency(event.target.value);
     };
 
-    const secondCurrencyChange = event => {
+    const secondCurrencyChange = (event) => {
         setSecondCurrency(event.target.value);
     };
 
-    const firstValueChange = e => {
-        setFirstValue(parseFloat(e.target.value));
+    const firstValueChange = (event) => {
+        setFirstValue(event.target.value);
     };
 
-    const secondValueChange = e => {
-        setSecondValue(parseFloat(e.target.value));
+    const secondValueChange = (event) => {
+        setSecondValue(event.target.value);
     };
 
+    // convert currencies value
     const convertFromTo = () => {
         const fromRate =
-            fromCurrency === "EUR" ? 1 : currencies.rates[fromCurrency];
+            firstCurrency === "EUR" ? 1 : currencies.rates[firstCurrency];
         const valueInEur = firstValue / fromRate;
         const toRate = secondCurrency === "EUR" ? 1 : currencies.rates[secondCurrency];
-        setSecondValue(valueInEur * toRate);
+        setSecondValue((valueInEur * toRate).toFixed(2));
     };
 
     const convertToFrom = () => {
         const toRate = secondCurrency === "EUR" ? 1 : currencies.rates[secondCurrency];
         const valueInEur = secondValue / toRate;
         const fromRate =
-            fromCurrency === "EUR" ? 1 : currencies.rates[fromCurrency];
-        setFirstValue(valueInEur * fromRate);
+            firstCurrency === "EUR" ? 1 : currencies.rates[firstCurrency];
+        setFirstValue((valueInEur * fromRate).toFixed(2));
     };
 
+
+    // if changig value or currency call convert functions
     useEffect(() => {
         convertFromTo();
          // eslint-disable-next-line
-    }, [firstValue, secondCurrency]);
-   
+    }, [firstValue]);
 
     useEffect(() => {
         convertToFrom();
         // eslint-disable-next-line
-    }, [secondValue, fromCurrency]);
-    
+    }, [firstCurrency]);
+
+    useEffect(() => {
+        convertToFrom();
+        // eslint-disable-next-line
+    }, [secondValue]);
+
+    useEffect(() => {
+        convertFromTo();
+         // eslint-disable-next-line
+    }, [secondCurrency]);
 
     if (!currencies) {
         return null;
     }
-
-
     return (
         <div className={classes.Currency}>
             <h1 className={classes.headliner}>React currency exchange</h1>
@@ -81,7 +91,7 @@ const Currency = () => {
                         ></input>
                         <select>
                             <option
-                                value={fromCurrency}
+                                value={firstCurrency}
                                 onChange={FirstCurrencyChange}
                             >EUR</option>
                             {Object.keys(currencies.rates).map((rate, index) => (
